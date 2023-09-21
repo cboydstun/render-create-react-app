bfc002559f80acf5102740257c6c5acdac4c89a6
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-09-21 14:58:17 -0500

    api call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default function App() {
+function App() {
+    const [people, setPeople] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        fetch("https://render-nodejs-api-zuwp.onrender.com/people")
+        // fetch("http://localhost:8080/people")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then(data => {
+                setPeople(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) return <p>Loading...</p> 
+
+    if (error) return <p>Error!</p> 
+
     return (
-        <div>HELLO LONDON COHORT</div>
+        <div>
+            <h1>People List</h1>
+            <ul>
+                {people.map(person => (
+                    <li key={person.name}>
+                        {person.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
+
+export default App;
